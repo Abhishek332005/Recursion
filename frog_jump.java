53d51ce9ae7bc861f3f8a3864bfaa5fadac4e4be
Abhishek Pratap Singh <ytabhishek721@gmail.com>
2025-09-16 23:43:59 +0530

    Add files via upload

---
+++ frog_jump.java
@@ -0,0 +1,22 @@
+package Recursion;
+import java.util.*;
+public class frog_jump {
+
+    static int best(int [] h, int n , int idx){
+        if(idx == n-1) return 0;
+
+        int op1 = Math.abs(h[idx+1] - h[idx]) + best(h,n,idx+1);
+        if(idx == n-2) return op1;
+
+        int op2 = Math.abs(h[idx+2] -h[idx]) + best(h,n,idx+2);
+
+        return Math.min(op1,op2);
+    }
+
+
+    public static void main(String[] args) {
+        int []h = {10,30,40,20};
+        System.out.println(best(h,h.length,0));
+
+    }
+}
